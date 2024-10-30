70e813b603ea20f91d668835100a14bfbeb6cd14
ytreeshan <44382924+ytreeshan@users.noreply.github.com>
2024-10-30 08:05:00 +0000

    not sure what happen will check again

--- nextjs-dashboard/app/lib/data.ts
+++ nextjs-dashboard/app/lib/data.ts
@@ -9,6 +9,7 @@ import {
 } from './definitions';
 import { formatCurrency } from './utils';
 
+
 export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
@@ -18,7 +19,8 @@ export async function fetchRevenue() {
     // await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
-
+    const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
+const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
     // console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
